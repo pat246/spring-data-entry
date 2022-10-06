5aa4943cfc48520f64779554e528396ba03f6c94
prashantnitdgp@gmail.com <prashantnitdgp@gmail.com>
2022-10-06 23:01:02 +0530

    clean up

--- demo-2/src/main/java/com/me/dataentry/repo/CountryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.me.dataentry.repo;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.me.dataentry.entity.Country;
-
-public interface CountryRepo extends PagingAndSortingRepository<Country, Long> {
-
-}
--- demo-2/src/main/java/com/me/dataentry/repo/PersonRepo.java
+++
@@ -1,9 +0,0 @@
-package com.me.dataentry.repo;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.me.dataentry.entity.Person;
-
-public interface PersonRepo extends PagingAndSortingRepository<Person, Long> {
-
-}
